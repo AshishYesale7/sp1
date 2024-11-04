9f7dc82ad29a0ddf4d1d41e89d038d7bb1cef660
Ashish  Vasant Yesale <67554129+AshishYesale7@users.noreply.github.com>
2024-11-05 02:01:52 +0530

    Delete github-xxuppj-hgzx1u directory

--- github-xxuppj-hgzx1u/src/components/LoadingSpinner.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function LoadingSpinner() {
-  return (
-    <div className="flex items-center justify-center py-8">
-      <div className="h-8 w-8 animate-spin rounded-full border-4 border-red-600 border-t-transparent"></div>
-    </div>
-  );
-}
